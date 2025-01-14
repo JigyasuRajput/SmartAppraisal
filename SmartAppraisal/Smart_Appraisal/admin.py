9722834707c894a4f5496d0410e59bd602aadef6
Jigyasu Rajput <jigyasu2021@gmail.com>
2025-01-14 06:30:58 +0530

    created a django superuser

--- SmartAppraisal/Smart_Appraisal/admin.py
+++ SmartAppraisal/Smart_Appraisal/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import (
+    User,
+    FacultyProfile,
+    AdminProfile,
+    Lecture,
+    Attendance,
+    SeminarWorkshop,
+    FacultyReport
+)
 
-# Register your models here.
+# Register models with basic admin setup
+admin.site.register(User)
+admin.site.register(FacultyProfile)
+admin.site.register(AdminProfile)
+admin.site.register(Lecture)
+admin.site.register(Attendance)
+admin.site.register(SeminarWorkshop)
+admin.site.register(FacultyReport)
--- SmartAppraisal/Smart_Appraisal/models.py
+++ SmartAppraisal/Smart_Appraisal/models.py
@@ -1,6 +1,30 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import BaseUserManager
+
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError("The Email field must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self.create_user(email, password, **extra_fields)
+
 
 class User(AbstractUser):
     ROLE_CHOICES = [
@@ -27,7 +51,11 @@ class User(AbstractUser):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
+    REQUIRED_FIELDS = []
 
+    objects = CustomUserManager()
+    
+    
 class FacultyProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.CharField(max_length=100)
